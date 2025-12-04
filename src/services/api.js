3becaf5289eb0d4b1db22f63e55e4287f4cd4d8f
psh.evolution <psh.evolution@gmail.com>
2025-12-04 09:10:26 -0300

    url do render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // backend rodando no Termux na porta 3000
+  baseURL: "https://backend-chat-fcvm.onrender.com/api", // backend rodando no Termux na porta 3000
   timeout: 10000,
 });
 
