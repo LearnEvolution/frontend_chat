a5bd08d1aea6ffe0db70f8bf8c983da1d2252e6f
psh.evolution <psh.evolution@gmail.com>
2025-12-04 11:18:03 -0300

    Resolve build Vercel: adiciona dedupe axios

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+plugins: [react()],
+resolve: {
+dedupe: ['axios'] // Garante que axios será incluído no bundle
+}
 })
