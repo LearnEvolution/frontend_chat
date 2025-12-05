1b6bdef0eea166f8559d69f347f6b440da8e5a09
psh.evolution <psh.evolution@gmail.com>
2025-12-04 22:26:25 -0300

    Corrigido build Vite + socket.io-client

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["socket.io-client"], // força o Vite a otimizar esse pacote
+    include: ["socket.io-client"], // força a otimização do socket.io-client
   },
   build: {
-    rollupOptions: {
-      external: [], // garante que não seja externalizado
-    },
     commonjsOptions: {
-      include: [/socket.io-client/, /node_modules/],
+      transformMixedEsModules: true, // essencial para Vercel
+      include: [/node_modules/, /socket.io-client/],
     },
   },
 });
